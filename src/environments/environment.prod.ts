1bff996af489c07a8a2f38f495130b82f619f427
EnilsonNeto <enilsonneto8@gmail.com>
2023-07-22 13:39:03 -0300

    atualização

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://us-central1-junkielabs-57977.cloudfunctions.net/apiPortfolio',
+  recaptcha: '6Lci95YiAAAAAOfkpaWwNSHiX5Ye7KT9ZOw-HYCv'
+};
